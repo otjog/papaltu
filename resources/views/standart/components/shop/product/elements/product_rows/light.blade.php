84ffcda5bc0f69b84408ff7dc5c65c0853838f50
otjog <fvirus@mail.ru>
2018-11-08 17:42:13 +0300

    Подшлифован макет и код html/css

---
+++ resources/views/standart/components/shop/product/elements/product_rows/light.blade.php
@@ -0,0 +1,73 @@
+<div class="row no-gutters">
+    @foreach( $products_row as $key => $product )
+        <div class="product-item-wrap product-item-light col-4 rounded-0 border-right border-bottom">
+        <!--span class="bg-light small px-1 py-1 w-50 rounded-bottom-right">Артикул: {{$product->scu or ''}}</span-->
+            <div class="product-item pb-1 pt-3 px-2">
+                <!-- Image-->
+                <div class="product-image">
+                    <a href="{{ route( 'products.show', $product->id ) }}">
+                        @if( isset($product->thumbnail) && file_exists( 'storage/img/shop/product/thumbnail/'.$product->thumbnail))
+                            <img class="img-fluid mx-auto d-block" src="{{ URL::asset('storage/img/shop/product/thumbnail/'.$product->thumbnail) }}">
+                        @else
+                            <div class="text-center text-light"><i class="fas fa-shopping-basket fa-7x"></i></div>
+                        @endif
+                    </a>
+                </div>
+                <!--Price-->
+                @if( isset($product->price['value']) && $product->price['value'] !== null)
+                    <div class="bg-white pt-3 pb-2">
+                        <div class="row">
+                            <div class="col-12 price category-product text-center">
+                                <div class="row">
+                                    @if( isset($product->price['sale']) && $product->price['sale'] > 0)
+                                        <div class="col-6">
+                                                        <span class="small">
+                                                            <s>
+                                                                {{$product->price['value'] + $product->price['sale']}}<small>{{$components['shop']['currency']['symbol']}}</small>
+                                                            </s>
+                                                        </span>
+                                        </div>
+                                    @endif
+                                    <div class="col text-danger">
+                                        <span>{{ $product->price['value']}}</span><small>{{$components['shop']['currency']['symbol']}}</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            @endif
+
+            <!--Name-->
+                <!--Name-->
+                <div class="product-name">
+                    <a class="text-dark" href="{{ route( 'products.show', $product->id ) }}">
+                        @isset($product->manufacturer['name'])
+                            {{ $product->manufacturer['name'] . ' ' }}
+                        @endisset
+                        {{ $product->name }}
+                        @if( isset($product->brands) && count($product->brands) > 0 && $product->brands !== null)
+                            @foreach($product->brands as $brand)
+                                {{ ' | ' . $brand->name}}
+                            @endforeach
+                        @endif
+                    </a>
+                </div>
+                <!--Action-->
+                <div class="product-action w-100 text-center border-top mt-2">
+                    @if( !isset($product->basket_parameters) || count($product->basket_parameters) === 0)
+                        <form method="post" role="form" action="{{route('baskets.store')}}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <input type="hidden" name="product_id"       value="{{ $product->id}}">
+                            <input type="hidden" name="quantity" value="1" >
+                            <input class="btn btn-link pb-0" type="submit" value="В корзину" />
+                        </form>
+                    @else
+                        <a class="pt-2 d-block" href="{{ route( 'products.show', $product->id ) }}">
+                            Выбрать размер
+                        </a>
+                    @endif
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
--- resources/views/standart/positions/header/default.blade.php
+++ resources/views/standart/positions/header/default.blade.php
@@ -1,13 +1,57 @@
 
     <!-- Top Bar -->
-
-    @include( $template_name .'.modules.header.top')
+    <div class="top_bar">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex flex-row">
+                    <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="{{ URL::asset('storage/img/elements/phone.png') }}" alt=""></div>{{$info['phone']}}</div>
+                    <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="{{ URL::asset('storage/img/elements/mail.png') }}" alt=""></div>{{$info['email']}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Header Main -->
+    <div class="header_main">
+        <div class="container">
+            <div class="row">
 
-    @include( $template_name .'.modules.header.main')
+                <!-- Logo -->
+                <div class="col-lg-3 col-sm-3 col-6 order-1">
+                    @include( $template_name .'.modules.logo.default')
+                </div>
+
+                <!-- Search -->
+                <div class="col-lg-6 col-12 order-lg-2 order-3 text-lg-left text-right">
+                    @include( $template_name .'.modules.search.default')
+                </div>
+
+                <!-- Cart -->
+                <div class="col-lg-3 col-6 order-lg-3 order-2 text-lg-left text-right">
+                    @include( $template_name .'.modules.shop_basket.default')
+                </div>
+
+            </div>
+        </div>
+    </div>
 
     <!-- Main Navigation -->
+    <nav class="main_nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
 
-    @include( $template_name .'.modules.header.nav')
+                    <div class="main_nav_content d-flex flex-row">
+
+                        <!-- Categories Menu -->
+                    @include( $template_name .'.modules.menu.shop', ['menu_type' => 'dropdown'])
+
+                    <!-- Main Nav Menu -->
+                        @include( $template_name .'.modules.menu.page', ['menu_type' => 'line', 'menu_name' => 'top_menu'])
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 
